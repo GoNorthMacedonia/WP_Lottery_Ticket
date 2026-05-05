09361f8286353b773fa8603d25174f61a35ce102
Joshua T <thecountryflagmaster@gmail.com>
2026-05-05 08:34:34 -0400

    finished scratch function and created randomize_win function

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-function scratch(cell){
-    cell = document.getElementById(`${cell}`)
-    cell.style.backgroundImage = 'url(i will put a backgroundimage in the files eventually)'
+function randomize_win(){
+    flag_pool = ['brunei', 'burundi', 'grenada', 'guyana', 'kuwait', 'kyrgyzstan',
+        'liechtenstein', 'malawi', 'marshall_islands', 'north_macedonia',
+        'sao_tome_and_principe', 'the_gambia', 'timor_leste', 'vanuatu'
+    ]
+
+    color_pool = ['#738678', 'slate_gray', 'gray']
+}
+
+function scratch(cell, background){
+    cell = document.getElementById(`${cell}`);
+    if (background /*check if background is an image*/){
+        cell.style.backgroundImage = background;
+    }
+
+    else{
+        cell.style.background_color
+    }
 }
