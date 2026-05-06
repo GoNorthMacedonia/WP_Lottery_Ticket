aec53bd3cc1ce24ccf46a7532ef2986cda4fec61
Joshua T <thecountryflagmaster@gmail.com>
2026-05-06 10:49:54 -0400

    added the randomizer for the win condition

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
-function randomize_win(){
+function randomize_win(cell){
     flag_pool = ['brunei', 'burundi', 'grenada', 'guyana', 'kuwait', 'kyrgyzstan',
         'liechtenstein', 'malawi', 'marshall_islands', 'north_macedonia',
-        'sao_tome_and_principe', 'the_gambia', 'timor_leste', 'vanuatu'
+        'sao_tome_and_principe', 'saint_kitts_and_nevis', 'the_gambia', 'timor_leste', 'vanuatu'
     ]
-
     color_pool = ['#738678', 'slate_gray', 'gray']
+
+    win = False
+
+    picker = Math.random() * 100;
+
+    if (Math.floor(picker) > 80){
+        if (Math.floor(picker) < 86){
+            win = True;
+        }
+    }
+
+    if (win == True){
+        while (picker >= 15){
+            picker = Math.random() * 100;
+        }
+
+        picker = Math.floor(picker);
+        background = flag_pool[picker]
+    }
+
+
+
+    cell = document.getElementById(cell)
+
+
 }
 
-function scratch(cell, background = None){
+function scratch(cell, background = None, multiplier = None){
     cell = document.getElementById(`${cell}`);
     if (background != None){
-        cell.style.backgroundImage = background;
+        mult = document.createElement('h5');
+        mult.textContent = multiplier;
+
+        cell.style.backgroundImage = 'background';
+        cell.appendChild(mult);
+        cell.style.justifyContent = 'spaceBetween';
     }
 
     else{
-        cell.style.background_color = #738678
+        cell.style.background_color = '#738678';
     }
 }
